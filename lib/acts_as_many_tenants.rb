b4f605f3e8206bfeb75625df9b42ca6b0e3db7f7
Laurens Nienhaus <l.nienhaus@gmail.com>
2013-01-27 16:38:47 +0100

    Implement :through option

--- lib/acts_as_many_tenants.rb
+++ lib/acts_as_many_tenants.rb
@@ -7,17 +7,17 @@ module ActsAsManyTenants
       
       if options[:through]
         has_many association, :through => options[:through]
+        # namespaced Model constant: foo/bar_baz -> Foo::BarBaz
+        reflection = options[:through].to_s.camelize.constantize.reflect_on_association(association)
+        # foo/bar_baz -> Foo::BarBaz -> BarBaz -> bar_baz -> :bar_baz
+        through_reflection = reflect_on_association(options[:through].to_s.camelize.demodulize.underscore.to_sym)
       else
         has_and_belongs_to_many association
+        reflection = reflect_on_association(association)
       end
 
-      # get the tenant model and its foreign key
-      reflection = reflect_on_association association
-# raise reflection.inspect
-      fkey = reflection.association_foreign_key
-
       if options[:immutable]
-        raise "TODO immutable association not yet implemented"
+        Rails.logger.info "TODO immutable association not yet implemented"
       end
 
       attr_accessible "#{association.to_s.singularize}_ids".to_sym # e.g. account_ids
@@ -27,18 +27,19 @@ module ActsAsManyTenants
       end
 
       # set the default_scope to scope to current tenant
+      # using EXISTS queries here, 
+      # not using joins() since that would give us ReadOnly records
       unless options[:through]
         default_scope lambda {
           if ActsAsTenant.current_tenant
-            # using EXISTS query here, 
-            # not using joins() since that would give us ReadOnly records
-            where("EXISTS (SELECT 1 from #{reflection.options[:join_table]} WHERE #{reflection.foreign_key} = #{self.table_name}.id AND #{fkey} = ?)", ActsAsTenant.current_tenant.id)
+            where("EXISTS (SELECT 1 FROM #{reflection.options[:join_table]} WHERE #{reflection.options[:join_table]}.#{reflection.foreign_key} = #{self.table_name}.id AND #{reflection.options[:join_table]}.#{reflection.association_foreign_key} = ?)", ActsAsTenant.current_tenant.id)
           end
         }
       else
-        raise "TODO default_scope for has_many :through associations"
         default_scope lambda {
-          # joins(options[:through]).where(options[:through] => {fkey => ActsAsTenant.current_tenant.id}) if ActsAsTenant.current_tenant
+          if ActsAsTenant.current_tenant
+            where("EXISTS (SELECT 1 FROM #{reflection.options[:join_table]} WHERE #{reflection.options[:join_table]}.#{reflection.foreign_key} = #{self.table_name}.#{through_reflection.foreign_key} AND #{reflection.options[:join_table]}.#{reflection.association_foreign_key} = ?)", ActsAsTenant.current_tenant.id)
+          end
         }
       end
     end
